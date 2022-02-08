91df395730fe166951787a39c5a6290a77d6d415
saiterala <36340387+saiterala@users.noreply.github.com>
2022-02-07 23:12:31 -0500

    Time: 68 ms (99.17%), Space: 45.3 MB (5.41%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var index =1
+    
+   for(let i=0; i < nums.length-1; i++){
+      if(nums[i]!= nums[i+1]){
+          nums[index++]= nums[i+1]
+      }
+   }
+    return index
+};
