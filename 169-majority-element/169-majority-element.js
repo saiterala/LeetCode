fc87f23819f3d3711e1926fe12799fc13abbe4c4
saiterala <36340387+saiterala@users.noreply.github.com>
2022-02-20 23:13:23 -0500

    Time: 102 ms (50.39%), Space: 43.2 MB (45.53%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -2,16 +2,37 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
-    let max = -Infinity
-    let result
-    const map ={}
-    for (let i = 0; i< nums.length; i++){
-        map[nums[i]] = (map[nums[i]] || 0) + 1
-       if(map[nums[i]] > max){
-           result = nums[i]
-           max = map[nums[i]]
-       }
+// var majorityElement = function(nums) {
+//     let max = -Infinity
+//     let result
+//     const map ={}
+//     for (let i = 0; i< nums.length; i++){
+//         map[nums[i]] = (map[nums[i]] || 0) + 1
+//        if(map[nums[i]] > max){
+//            result = nums[i]
+//            max = map[nums[i]]
+//        }
+//     }
+//     return result
+// };
+
+
+// Moore's voting algorithm
+
+var majorityElement = function(nums){
+    let count = 1
+    let major = nums[0]
+    
+    for (let i=1; i< nums.length; i++){
+        if(nums[i] === major){
+            count++
+        }else count--
+        
+        if(count === 0){
+            major = nums[i]
+            count = 1
+        }
     }
-    return result
-};
+    
+    return major
+}
