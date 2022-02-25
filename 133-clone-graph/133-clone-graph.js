08696028a869790f37e1daad5ac7c065551c3379
saiterala <36340387+saiterala@users.noreply.github.com>
2022-02-24 21:07:21 -0500

    Time: 72 ms (85.62%), Space: 45 MB (5.23%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -40,3 +40,19 @@ var cloneGraph = function(node) {
     }
     return map.get(node)
 };
+
+//Rescursive Approach
+// var cloneGraph = function(node) {
+//     let res = {};
+    
+//     const clone = node => {
+//         if (!node) return node;
+//         if(!res[node.val]) {
+//             res[node.val] = new Node(node.val);
+//             res[node.val].neighbors = node.neighbors.map((neighbor)=>clone(neighbor))
+//         } 
+//         return res[node.val];
+//     }
+    
+//     return clone(node);
+// };
