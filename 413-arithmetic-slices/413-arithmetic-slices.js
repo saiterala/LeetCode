faad217cc9bc4b716e89fa4932f9450d9a7ccada
saiterala <36340387+saiterala@users.noreply.github.com>
2022-03-02 23:27:18 -0500

    Time: 78 ms (64.44%), Space: 41.8 MB (48.89%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.js
+++ 413-arithmetic-slices/413-arithmetic-slices.js
@@ -7,33 +7,19 @@ var numberOfArithmeticSlices = function(nums) {
     if(nums.length < 3) return result
     let i = 2
     let preDiff = nums[0] - nums[1]
-    let currCount = 2
+    let currCount = 0
     while( i < nums.length){
         let diff = nums[i-1] - nums[i]
         if(diff === preDiff){
-            currCount++
+            result += ++currCount
         }else{
             preDiff = diff
-            result += getTotalCount(currCount)  
-            currCount = 2
+            currCount = 0
         }
         i++
     }
-    result += getTotalCount(currCount)
     
     return result
 };
 
-const getTotalCount = (n) => {
-    console.log(n)
-    if(n < 3) return 0
-    if(n === 3) return 1
-    return getTotalCount(n-1) + (n-2)
-}
 
-//3. --> 1
-//4 ---> 3
-//5 ---> 6
-//6 ---> 10
-//7 ---> 15
-//8 ---> 21
