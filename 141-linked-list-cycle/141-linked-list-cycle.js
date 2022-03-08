000e9f045aa533d131166dc021354fe56f2bb06c
saiterala <36340387+saiterala@users.noreply.github.com>
2022-03-07 21:51:44 -0500

    Time: 135 ms (18.08%), Space: 45.3 MB (23.23%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -15,7 +15,7 @@ var hasCycle = function(head) {
     let slow = head
     let fast = head
     
-    while(slow.next && fast.next?.next){
+    while(fast.next?.next){
         slow = slow.next
         fast = fast.next.next
         if(slow === fast) return true
