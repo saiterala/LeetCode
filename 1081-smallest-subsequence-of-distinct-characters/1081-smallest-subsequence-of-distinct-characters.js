7e3d39615603bed9b7c5623b868d312b0935602a
saiterala <36340387+saiterala@users.noreply.github.com>
2022-03-17 23:38:35 -0400

    Time: 104 ms (48.57%), Space: 42.6 MB (80.00%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var smallestSubsequence = function(s) {
+    const lastSeen = {}
+    const lastIndex = {}
+    const stack =[]
+    
+    for ( let i =0; i < s.length; i++){
+        lastIndex[s[i]] = i
+    }
+    
+    for ( let i =0; i < s.length ; i++){
+        const char = s[i]
+        if(lastSeen[char]) continue
+        while(stack.length && stack[stack.length - 1] > char && i < lastIndex[stack[stack.length - 1]]){
+            const value = stack.pop()
+            lastSeen[value] = false
+        }
+        stack.push(s[i])
+        lastSeen[s[i]] = true
+    }
+    return stack.join('')
+};
