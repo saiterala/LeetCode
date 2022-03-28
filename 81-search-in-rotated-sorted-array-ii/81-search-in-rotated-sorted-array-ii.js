b118da9f51a1e9ddc791155e0e281d9797d938de
saiterala <36340387+saiterala@users.noreply.github.com>
2022-03-28 00:53:06 -0400

    Time: 85 ms (54.26%), Space: 42.7 MB (43.19%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.js
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.js
@@ -11,14 +11,18 @@ var search = function(nums, target) {
         if(target === nums[left] || target === nums[right]) return true
         let middle = Math.floor((left + right) / 2)
         if(target === nums[middle]) return true
-        if(nums[left] === nums[middle]) left++
-        else if(nums[middle] === nums[right]) right--
-        else if(nums[left] < nums[middle]){
-            if(target > nums[left] && target < nums[middle] ) right = middle - 1
+        // if(nums[left] === nums[middle]) left++
+        // else if(nums[middle] === nums[right]) right--
+        if(nums[left] === nums[middle] && nums[middle] === nums[right]){
+            left++
+            right--
+        }
+        else if(nums[left] <= nums[middle]){
+            if(target >= nums[left] && target < nums[middle] ) right = middle - 1
             else left = middle + 1
         }
-        else if(nums[middle] < nums[right]){
-            if(target > nums[middle] && target < nums[right] ) left = middle + 1
+        else if(nums[middle] <= nums[right]){
+            if(target > nums[middle] && target <= nums[right] ) left = middle + 1
             else right = middle - 1
         }
     }
