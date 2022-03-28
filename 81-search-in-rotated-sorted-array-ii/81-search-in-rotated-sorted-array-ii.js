ee7ca6a41364b4659cf0822b57ffb94075ca1c60
saiterala <36340387+saiterala@users.noreply.github.com>
2022-03-28 00:47:22 -0400

    Time: 89 ms (48.28%), Space: 42.7 MB (43.19%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    
+    while(left <= right){
+        if(target === nums[left] || target === nums[right]) return true
+        let middle = Math.floor((left + right) / 2)
+        if(target === nums[middle]) return true
+        if(nums[left] === nums[middle]) left++
+        else if(nums[middle] === nums[right]) right--
+        else if(nums[left] < nums[middle]){
+            if(target > nums[left] && target < nums[middle] ) right = middle - 1
+            else left = middle + 1
+        }
+        else if(nums[middle] < nums[right]){
+            if(target > nums[middle] && target < nums[right] ) left = middle + 1
+            else right = middle - 1
+        }
+    }
+    
+    return false
+};
