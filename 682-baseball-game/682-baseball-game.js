6e46555bfd5e3e9676305cf1cca915d1fcbbcd6f
saiterala <36340387+saiterala@users.noreply.github.com>
2022-04-09 22:14:34 -0400

    Time: 83 ms (56.77%), Space: 44.9 MB (10.15%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} ops
+ * @return {number}
+ */
+var calPoints = function(ops) {
+    let resultStack = []
+    
+    for (const str of ops){
+        const x = parseInt(str)
+        let length = resultStack.length
+        if(str === '+'){
+            resultStack.push(resultStack[length -1 ] + resultStack[length - 2])
+        }else if( str === 'D'){
+            resultStack.push(2 * resultStack[length - 1])
+        }else if(str === 'C') resultStack.pop()
+        else resultStack.push(x)
+    }
+    console.log(resultStack)
+    return resultStack.reduce((a,b) => a + b)
+    
+};
