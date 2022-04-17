8bdb3fc7b4c493758a86d10cc51a5a08e677207b
saiterala <36340387+saiterala@users.noreply.github.com>
2022-04-17 16:38:59 -0400

    Time: 64 ms (82.64%), Space: 42.2 MB (50.17%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -4,10 +4,8 @@
  */
 var plusOne = function(digits) {
     const string = digits.join('')
-    console.log(BigInt(string))
     let num = BigInt(string)
     num++
-    console.log(num)
     
     return num.toString().split('')
 };
