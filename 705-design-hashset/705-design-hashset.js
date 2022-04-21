db9078824258d7879390176501dbfc2f3c00c969
saiterala <36340387+saiterala@users.noreply.github.com>
2022-04-20 23:11:43 -0400

    Time: 191 ms (76.23%), Space: 53.1 MB (47.13%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.js
@@ -0,0 +1,39 @@
+
+var MyHashSet = function() {
+     this.customSet = {}
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+   this.customSet[key] = key
+    
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+   delete this.customSet[key]
+    
+};
+
+/** 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    const result = key in this.customSet
+    return result
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
