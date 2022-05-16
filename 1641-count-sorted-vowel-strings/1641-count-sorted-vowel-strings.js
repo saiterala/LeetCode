4b1177c38bac3c3241b8f232338f0f3559c331dc
saiterala <36340387+saiterala@users.noreply.github.com>
2022-05-15 23:33:42 -0400

    Time: 630 ms (12.92%), Space: 42.6 MB (21.61%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
@@ -20,3 +20,27 @@ const helper = (start, n, temp, result, str) => {
         temp.pop()
     }
 }
+
+
+// var countVowelStrings = function(n) {
+//     const vowels = ["a","e","i","o","u"]
+//     let count = 0
+    
+//     function generateComb(s, start){
+//         // base case: s length is n => increment the count
+//         if (s.length === n) {
+//             count += 1
+//             return
+//         }
+        
+//         // going over the vowels and append the s
+//         // i start from start to prevent generating strings that are not lexicographically sorted
+//         for (let i = start; i < 5; i++){
+//             generateComb(s+vowels[i], i)
+//         }
+//     }
+    
+//     generateComb('', 0)
+    
+//     return count
+// };
