ae493fcbfce6d5a5655508b66add0ad9bd120ce5
saiterala <36340387+saiterala@users.noreply.github.com>
2023-10-11 15:18:14 -0400

    Time: 101 ms, Memory: 46.7 MB - LeetHub

---
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const map = {}
+    
+    for (const char of s){
+        map[char] = map[char] ? map[char] + 1 : 1
+    }
+    console.log(map)
+    
+    for(let i=0; i < s.length ; i++ ){
+        if(map[s[i]] === 1){
+            return i
+        }
+    }
+    return -1
+};
