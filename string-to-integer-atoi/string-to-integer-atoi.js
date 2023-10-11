4e5c2cefefb8be9788768fe60e75eacd322374f2
saiterala <36340387+saiterala@users.noreply.github.com>
2023-10-11 16:39:56 -0400

    Time: 160 ms, Memory: 51.8 MB - LeetHub

---
+++ string-to-integer-atoi/string-to-integer-atoi.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    
+    s= s.trim().toLowerCase()
+    const res= []
+    let multiplier = 1
+    
+    for(i = 0 ; i < s.length ; i++){
+        if(i === 0){
+             if(s[i] === "-"){
+                multiplier = -1
+                continue;
+            }
+            if(s[i] === "+"){
+                continue;
+            }
+        }
+         const val = parseInt(s[i])
+         if(val >= 0 && val <= 9){
+            console.log("valid ", s[i])
+            res.push(s[i])
+        } else break;
+    }
+    
+    console.log(res)
+    if(!res.length) return 0
+    let val = parseInt(res.join("")) 
+    val = val * multiplier
+    console.log(val)
+    if( val > (Math.pow(2, 31) - 1)) return Math.pow(2, 31) -1
+    if( val < -Math.pow(2, 31)) return -Math.pow(2, 31)
+    return val
+};
