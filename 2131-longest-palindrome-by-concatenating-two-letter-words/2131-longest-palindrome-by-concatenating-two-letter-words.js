80e2d067408ab11df7b564f587186e242db91caf
saiterala <36340387+saiterala@users.noreply.github.com>
2023-10-18 16:22:47 -0400

    Time: 318 ms (29.63%), Space: 68.1 MB (11.11%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var longestPalindrome = function(words) {
+    let count = 0
+    const map = new Map()
+    const cmap = {}
+    for(const word of words){
+        const rev = word.split("").reverse().join("")
+        if(map.has(rev)){
+            count = count + 2
+            if(map.get(rev) === 1){
+                map.delete(rev)    
+            }else {
+                map.set(rev, map.get(rev) - 1)
+            } 
+        }else if(word === rev){
+            cmap[word] = (cmap[word] || 0) + 1
+        }
+        else{
+            map.set(word, (map.get(word) || 0) + 1)
+        }
+    }
+    console.log(count)
+    console.log(cmap)
+    let central = false
+    Object.values(cmap).forEach( val => {
+        if(val >= 2){
+            count= count + (Math.floor(val/2) * 2)
+        }
+        if(val % 2 === 1){
+            central = true
+        }
+    })
+    
+    if(central) count++
+    return count * 2
+};
