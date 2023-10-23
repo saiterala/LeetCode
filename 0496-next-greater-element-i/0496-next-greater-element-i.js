97ed3663d55e2f245c97b4a16bc99fae848cc368
saiterala <36340387+saiterala@users.noreply.github.com>
2023-10-23 17:56:29 -0400

    Time: 68 ms (15.87%), Space: 45.2 MB (12.96%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    const stack = []
+    const map = {}
+    const res = []
+    
+    for(let i = 0; i < nums2.length ; i++){
+        
+        while(stack.length && nums2[stack[stack.length - 1]] < nums2[i]){
+            map[nums2[stack.pop()]] = nums2[i]
+        }
+        stack.push(i)
+    }
+    for(let i = 0; i < nums1.length; i++){
+        if(nums1[i] in map){
+            res.push(map[nums1[i]])
+        }else res.push(-1)
+    }
+    
+    console.log(map)
+    return res
+    
+};
