1b14cecf50c46c42fc12d62f63fca9ede95e745c
saiterala <36340387+saiterala@users.noreply.github.com>
2023-12-04 23:55:13 -0500

    Time: 51 ms (57.39%), Space: 42.1 MB (39.02%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let l = 0
+    let r = nums.length -1
+    
+    while(l < r){
+        const mid = Math.floor((l + r)/2)
+        if(nums[mid] > nums[mid + 1]){
+            r = mid
+        }else {
+            l = mid + 1
+        }
+    }
+    
+    return l
+};
