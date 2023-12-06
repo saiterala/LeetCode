27e49fccab1e666f52f1b035c2ea99d5ff2ddf52
saiterala <36340387+saiterala@users.noreply.github.com>
2023-12-05 22:15:13 -0500

    Time: 98 ms (5.09%), Space: 45.9 MB (5.09%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    let i = nums.length - 2
+    
+    while ( i >= 0 && nums[i] >= nums[i+1]){
+        i--
+    }
+    
+    if(i >= 0){
+        let j = nums.length -1
+        
+        while(nums[j] <= nums[i]){
+            j--
+        }
+        swap(nums, i , j)
+    }
+    console.log(nums)
+    reverse(nums, i+1)
+    return nums
+};
+
+const reverse = (nums, start) => {
+    let i = start
+    let j = nums.length -1
+    
+    while ( i < j){
+        swap(nums, i , j)
+        i++
+        j--
+    }
+}
+
+const swap = (nums, i , j) => {
+    const temp = nums[i]
+    nums[i] = nums[j]
+    nums[j] = temp
+}
