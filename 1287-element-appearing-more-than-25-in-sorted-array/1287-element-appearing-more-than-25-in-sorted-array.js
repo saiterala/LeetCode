72e12f815cca83d865bfc5c8ed5290f517957471
saiterala <36340387+saiterala@users.noreply.github.com>
2024-01-05 14:25:53 -0500

    Time: 60 ms (42.69%), Space: 45.4 MB (22.54%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function(arr) {
+    const max = Math.floor(arr.length / 4)
+    console.log(max)
+    
+    const map = {}
+    for (const char of arr){
+        map[char] = (map[char] || 0) + 1
+        if(map[char] > max) return char
+    }
+};
