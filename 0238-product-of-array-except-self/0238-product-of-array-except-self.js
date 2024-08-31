4d645611990111bd8e24bc72cfa5cd2b04b9faa6
saiterala <36340387+saiterala@users.noreply.github.com>
2024-08-31 15:41:36 -0400

    Time: 100 ms (68.92%), Space: 61.8 MB (87.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -15,10 +15,15 @@ var productExceptSelf = function(nums) {
         rightProduct[i] = rightProduct[i + 1] * nums[i+1]
     }
    
-
-    let result = []
-    for(let i = 0; i < nums.length; i ++){
-        result[i] = leftProduct[i] * rightProduct[i]
+    let r = 1
+    for(let i = nums.length -1; i >=0; i--){
+        leftProduct[i] = r * leftProduct[i]
+        r = r * nums[i]
     }
-    return result
+
+    // let result = []
+    // for(let i = 0; i < nums.length; i ++){
+    //     result[i] = leftProduct[i] * rightProduct[i]
+    // }
+    return leftProduct
 };
